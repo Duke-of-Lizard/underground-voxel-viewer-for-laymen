b0d26c2ce8d548c42430a408497788c056e6389a
Martijn <mblijswijk@amberg.ch>
2025-02-08 21:27:26 +0100

    update to pts

---
+++ backend/specklePusher.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+const { NetCDFReader } = require("netcdfjs");
+
+const outputFolder = "../output";
+const outputFilename = "output.vv";
+const filePath =
+  "C:/Users/mblijswijk/VS projects/underground-voxel-viewer-for-laymen/data/FRE16/netcdf/2025-02-08-FRE16-sensitive_clay_probability-subset.nc";
+
+const tilesetTemplate = {
+  asset: {
+    version: "1.1",
+  },
+  root: {
+    boundingVolume: {
+      box: [0, 0, 0, 50, 0, 0, 0, 50, 0, 0, 0, 50], // Adjust based on your dataset
+    },
+    geometricError: 500,
+    refine: "ADD",
+    content: {
+      uri: outputFilename,
+    },
+  },
+};
+
+
+// Read the file as a Buffer
+const data = fs.readFileSync(filePath);
+const reader = new NetCDFReader(data);
+
+// List available variables
+console.log(
+  "Variables in NetCDF:",
+  reader.variables.map((v) => v.name)
+);
+
+// Extract grid dimensions
+const x_dim = reader.dimensions.find((d) => d.name === "x").size;
+const y_dim = reader.dimensions.find((d) => d.name === "y").size;
+const z_dim = reader.dimensions.find((d) => d.name === "z").size;
+
+const x = reader.getDataVariable("x")
+const y = reader.getDataVariable("y")
+const z = reader.getDataVariable("z")
+
+//console.log(x)
+//console.log(y)
+//console.log(z)
+let resistivity = reader.getDataVariable("resistivity");
+const probability_sensitive_clay = reader.getDataVariable(
+  "probability_sensitive_clay"
+);
+
+//resistivity = resistivity.filter((v) => v >= 0)
+const indicesToKeep = resistivity.map((item, index) => item >= 0 ? index : -1).filter(index => index !== -1);
+
+const points = []
+
+for (let i_x=0; i_x < x.length; i_x++){
+    for (let i_y=0; i_y < y.length; i_y++){
+        for (let i_z=0; i_z < z.length; i_z++){
+            points.push({
+                x: x[i_x],
+                y: y[i_y],
+                z: z[i_z],
+            })
+        }
+    }
+}
+
+
+const filteredPoints = indicesToKeep.map(index => points[index]);
+
+// console.log(points)
+// console.log(resistivity)
+console.log(filteredPoints)
+
+
+// --------------------
+
+
+function convertListToJson(list) {
+    return JSON.stringify(list);
+}
+
+const jsonString = convertListToJson(filteredPoints);
+// console.log(jsonString); // Output the JSON string
+
+// To demonstrate parsing it back:
+const parsedList = JSON.parse(jsonString);
+// console.log(parsedList); // Output the JavaScript object
+
+//  If you want formatted/pretty printed JSON:
+const prettyJsonString = JSON.stringify(filteredPoints, null, 2); // The '2' indents with 2 spaces
+//console.log(prettyJsonString);
+
+// Specify the file path where you want to save the JSON
+const filePathJson = './pts.json'; 
+
+// Write the JSON string to the file
+fs.writeFile(filePathJson, jsonString, (err) => {
+  if (err) {
+    console.error('Error writing to file:', err);
+  } else {
+    console.log('JSON data saved to', filePathJson);
+  }
+});
