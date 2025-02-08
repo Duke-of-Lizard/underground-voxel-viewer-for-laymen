11c4eea39431438afa0b51a3a973a5c16421a0d5
Sven Rüegg <svenrue@gmx.ch>
2025-02-08 22:53:24 +0100

    add utm package, switch to relative path

--- backend/specklePusher.js
+++ backend/specklePusher.js
@@ -4,7 +4,7 @@ const { NetCDFReader } = require("netcdfjs");
 const outputFolder = "../output";
 const outputFilename = "output.vv";
 const filePath =
-  "C:/Users/mblijswijk/VS projects/underground-voxel-viewer-for-laymen/data/FRE16/netcdf/2025-02-08-FRE16-sensitive_clay_probability-subset.nc";
+  "../data/FRE16/netcdf/2025-02-08-FRE16-sensitive_clay_probability-subset.nc";
 
 const tilesetTemplate = {
   asset: {
@@ -22,7 +22,6 @@ const tilesetTemplate = {
   },
 };
 
-
 // Read the file as a Buffer
 const data = fs.readFileSync(filePath);
 const reader = new NetCDFReader(data);
@@ -38,9 +37,9 @@ const x_dim = reader.dimensions.find((d) => d.name === "x").size;
 const y_dim = reader.dimensions.find((d) => d.name === "y").size;
 const z_dim = reader.dimensions.find((d) => d.name === "z").size;
 
-const x = reader.getDataVariable("x")
-const y = reader.getDataVariable("y")
-const z = reader.getDataVariable("z")
+const x = reader.getDataVariable("x");
+const y = reader.getDataVariable("y");
+const z = reader.getDataVariable("z");
 
 //console.log(x)
 //console.log(y)
@@ -51,47 +50,53 @@ const probabilitySensitiveClay = reader.getDataVariable(
 );
 
 //resistivity = resistivity.filter((v) => v >= 0)
-const indicesToKeep = resistivity.map((item, index) => item >= 0 ? index : -1).filter(index => index !== -1);
+const indicesToKeep = resistivity
+  .map((item, index) => (item >= 0 ? index : -1))
+  .filter((index) => index !== -1);
 
-const points = []
+const points = [];
 
-for (let i_x=0; i_x < x.length; i_x++){
-    for (let i_y=0; i_y < y.length; i_y++){
-        for (let i_z=0; i_z < z.length; i_z++){
-            points.push({
-                x: x[i_x],
-                y: y[i_y],
-                z: z[i_z],
-            })
-        }
+for (let i_x = 0; i_x < x.length; i_x++) {
+  for (let i_y = 0; i_y < y.length; i_y++) {
+    for (let i_z = 0; i_z < z.length; i_z++) {
+      points.push({
+        x: x[i_x],
+        y: y[i_y],
+        z: z[i_z],
+      });
     }
+  }
 }
 
-
-const filteredPoints = indicesToKeep.map(index => points[index]);
-const filteredResistivity = indicesToKeep.map(index => resistivity[index]);
-const filteredProbability = indicesToKeep.map(index => probabilitySensitiveClay[index]);
+const filteredPoints = indicesToKeep.map((index) => points[index]);
+const filteredResistivity = indicesToKeep.map((index) => resistivity[index]);
+const filteredProbability = indicesToKeep.map(
+  (index) => probabilitySensitiveClay[index]
+);
 
 // console.log(points)
 // console.log(resistivity)
 //console.log(filteredPoints)
 
-
 // --------------------
 
-
-const utm = require('utm');
+const utm = require("utm");
 
 // Convert from UTM to WGS84
 function convertUtmToWgs84(easting, northing) {
-    // The utm.to_latlon function expects:
-    // easting, northing, zone number, zone letter (calculated automatically)
-    const coords = utm.toLatLon(easting, northing, zoneNum=32, zoneLetter="X");
-    
-    return {
-        latitude: coords.latitude,
-        longitude: coords.longitude
-    };
+  // The utm.to_latlon function expects:
+  // easting, northing, zone number, zone letter (calculated automatically)
+  const coords = utm.toLatLon(
+    easting,
+    northing,
+    (zoneNum = 32),
+    (zoneLetter = "X")
+  );
+
+  return {
+    latitude: coords.latitude,
+    longitude: coords.longitude,
+  };
 }
 
 // // Convert from WGS84 to UTM
@@ -100,25 +105,24 @@ function convertUtmToWgs84(easting, northing) {
 //     // { easting, northing, zoneNum, zoneLetter }
 //     return utm.from_latlon(latitude, longitude);
 // }
-const translatedPoints = []
-for (let i=0; i<filteredPoints.length;i++){
-    const xy = convertUtmToWgs84(filteredPoints[i].x, filteredPoints[i].y)
-    translatedPoints.push({
-        x: xy["longitude"],
-        y: xy["latitude"],
-        z: filteredPoints[i].z,
-        resistivity: filteredResistivity[i],
-        probability: filteredProbability[i]
-    }
-    )
+const translatedPoints = [];
+for (let i = 0; i < filteredPoints.length; i++) {
+  const xy = convertUtmToWgs84(filteredPoints[i].x, filteredPoints[i].y);
+  translatedPoints.push({
+    x: xy["longitude"],
+    y: xy["latitude"],
+    z: filteredPoints[i].z,
+    resistivity: filteredResistivity[i],
+    probability: filteredProbability[i],
+  });
 }
 
-console.log(translatedPoints)
+console.log(translatedPoints);
 
 // ---------------------
 
 function convertListToJson(list) {
-    return JSON.stringify(list);
+  return JSON.stringify(list);
 }
 
 const jsonString = convertListToJson(filteredPoints);
@@ -133,13 +137,13 @@ const prettyJsonString = JSON.stringify(filteredPoints, null, 2); // The '2' ind
 //console.log(prettyJsonString);
 
 // Specify the file path where you want to save the JSON
-const filePathJson = './pts.json';
+const filePathJson = "./pts.json";
 
 // Write the JSON string to the file
 fs.writeFile(filePathJson, jsonString, (err) => {
   if (err) {
-    console.error('Error writing to file:', err);
+    console.error("Error writing to file:", err);
   } else {
-    console.log('JSON data saved to', filePathJson);
+    console.log("JSON data saved to", filePathJson);
   }
 });
